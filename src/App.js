a78b4df7035494878eb7c1faf26e82386383e4e4
nguyenxuanhoat9 <nguyenxuanhoat9a@gmail.com>
2021-08-29 03:11:37 +0700

    onclick event

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react"
 
 function App() {
+  const [smImage, setSmImage]= useState({
+    active: 0,
+    srcImg: ["images/img-1.jpg","images/img-2.jpg","images/img-3.jpg"]
+  })
+
+  function toggleActive(index) {
+    setSmImage({...smImage, active: index})
+  }
+  function activeClass(index){
+    if(smImage.active == index){
+      return "active"
+    }
+    else return ""
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="products">
+      <div id="large">
+        <img src={smImage.srcImg[smImage.active]} />
+      </div>
+
+      <div id="small">
+        {smImage.srcImg.map((element, index)=>(
+          <img key = {index}
+          className={activeClass(index)}
+          onClick={ ()=>{
+            toggleActive(index)
+          }}
+          src={element} />
+        ))}
+      </div>
     </div>
+
   );
 }
 
