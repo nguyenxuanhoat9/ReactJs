4b5e9b73871d4f0d7f25ede0aa708cd0f81e0285
Nguyễn Xuân Hoạt <nguyenxuanhoat9a@gmail.com>
2021-11-11 02:19:40 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+
+const App = () => {
+  const [data, setData] = React.useState([])
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "https://jsonplaceholder.typicode.com/users",
+      data: {}
+    }).then((res) => {
+      setData(res.data)
+      console.log(res.data)
+    }).catch((err) => {
+      console.log(err.data)
+    })
+
+  }, [])
 
 
-function App() {
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div id="wrapper" className="container">
+      <h2>Quản lý thành viên</h2>
+      <p>Demo đơn giản về hiển thị danh sách thành viên thông qua việc <b>GET Data API bằng Axios Module</b></p>
+      <table className="table table-dark table-hover">
+        <thead>
+          <tr>
+            <th>Fullname</th>
+            <th>Username</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+{
+  data.map(user =>
+    <tr>
+    <td>{user.name}</td>
+    <td>{user.username}</td>
+    <td>{user.email}</td>
+  </tr>
+  )
+}
+
+        </tbody>
+      </table>
     </div>
-  );
+
+  )
 }
 
 export default App;
